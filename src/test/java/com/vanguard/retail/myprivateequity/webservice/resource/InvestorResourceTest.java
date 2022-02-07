95296114abd843f850d9e953118efa4876667097
abhijeet <abhijeet.thakur1@gmail.com>
2022-02-07 18:18:00 +0530

    second commit

--- src/test/java/com/vanguard/retail/myprivateequity/webservice/resource/InvestorResourceTest.java
+++ src/test/java/com/vanguard/retail/myprivateequity/webservice/resource/InvestorResourceTest.java
@@ -20,7 +20,7 @@ import com.vanguard.retail.myprivateequity.webservice.service.InvestorService;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
-
+import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Category(JUnitTestRule.class)
